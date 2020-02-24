01635406bf566406e0fdb2d03f0d282dcad58721
brianwiggins <peppermint.butl3r@gmail.com>
2020-02-24 10:09:43 -0700

    default closed=false

--- server/models/Bug.js
+++ server/models/Bug.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Bug = new Schema({
-  closed: { type: Boolean },
-  description: { type: String },
-  title: { type: String },
+  closed: { type: Boolean, default: false },
+  description: { type: String, required: true },
+  title: { type: String, required: true },
   reportedBy: { type: String },
   lastModified: { type: Date }
 }, { timestamps: true, toJSON: { virtuals: true } })
