6c42c5b0327b974d613ae366320104048c3fd8bc
brianwiggins <peppermint.butl3r@gmail.com>
2020-02-24 09:16:06 -0700

    a

--- server/controllers/BugController.js
+++ server/controllers/BugController.js
@@ -8,6 +8,10 @@ export default class BugController {
       .Router()
       //NOTE  each route gets registered as a .get, .post, .put, or .delete, the first parameter of each method is a string to be concatinated onto the base url registered with the route in main. The second parameter is the method that will be run when this route is hit.
       .get("", this.getAll)
+      .get("/:id", this.getById)
+      .post("", this.create)
+      .get("/:id/note", this.getnotesByBugId)
+      .put("", this.edit)
   }
 
   async getAll(req, res, next) {
